8321aeee1faa940d07b97d087b5e41bba6d96f3d
tales1982 <tales_lima_1982@hotmail.com>
2025-04-09 14:55:56 +0200

    FACULDADE PUCRS

--- EXERCICIOS/FACULDADE_PUCRS/08-Carro/include/Carro.hpp
+++ EXERCICIOS/FACULDADE_PUCRS/08-Carro/include/Carro.hpp
@@ -6,7 +6,33 @@
 /*   By: tales <tales@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 13:26:29 by tales             #+#    #+#             */
-/*   Updated: 2025/04/09 13:26:30 by tales            ###   ########.fr       */
+/*   Updated: 2025/04/09 14:51:06 by tales            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CARRO_HPP
+#define CARRO_HPP
+
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+class Carro
+{
+private:
+    float tanque;
+    float consumo;
+    
+public:
+    Carro();
+    Carro(/* args */);
+    Carro(const Carro &other);
+    Carro &operator=(const Carro &other);
+    ~Carro();
+    
+    
+};
+
+
+
+#endif
